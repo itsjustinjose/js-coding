4f7f9305daa0c2d193fea1c641da11af9b6c6607
Justin-SETU <20110497@mail.wit.ie>
2025-01-23 16:03:46 +0000

    operators

---
+++ basics/boolean.js
@@ -0,0 +1,12 @@
+console.log(3>2)
+
+let abc="JUSTIN", cba="jose";
+console.log("cba"<"abc")
+console.log(8*null)
+console.log("5"-1)
+console.log("5"+1)
+console.log("five"+1)
+console.log("five"*1)
+console.log(false==0)
+console.log(false!=0)
+console.log(null == undefined)
---
+++ basics/string.js
@@ -0,0 +1,7 @@
+let x = 'This is the first line \n This is second line';
+let y= "A new line character with \\ is included like \\ "
+let z= `template literal is half of 100 is ${100/2}`
+
+console.log(x)
+console.log(y)
+console.log(z)
